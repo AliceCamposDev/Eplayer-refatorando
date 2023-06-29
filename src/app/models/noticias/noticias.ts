381d68bddb3049e2a0189583201b62f724fb57c6
Gabriel Campos <gabrielfcampos2017@gmail.com>
2023-06-28 23:42:03 -0300

    adcionando noticias

---
+++ src/app/models/noticias/noticias.ts
@@ -0,0 +1,8 @@
+export interface Noticias {
+    titulo: string;
+    descricao: string;
+    imagem: string;
+    data: string;
+    link: string;
+    id?: number;
+}
--- src/app/templates/card-noticias/card-noticias.component.ts
+++ src/app/templates/card-noticias/card-noticias.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { Noticias } from 'src/app/models/noticias/noticias';
+import { NoticiasService } from 'src/app/services/noticias/noticias.service';
+
 @Component({
   selector: 'app-card-noticias',
   templateUrl: './card-noticias.component.html',
   styleUrls: ['./card-noticias.component.css']
 })
 export class CardNoticiasComponent {
+  constructor(private noticiaService: NoticiasService) { }
 
+  listaNoticias = [] as Noticias[]
 
   ngOnInit(): void {
+    this.carregarNoticias()
   }
 
-  
-  
+  carregarNoticias() {
+    this.noticiaService.getNoticias().subscribe((noticiasRecebidas: Noticias[]) => {
+      this.listaNoticias = noticiasRecebidas
+      console.log(this.listaNoticias);
+    })
+
+  }
 }
